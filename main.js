8abe08711fc1892899defaf3a82ff889101e6eda
Gabbyroba <gabbyramosbr2@gmail.com>
2023-09-18 16:46:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+// URL da API
 const apiUrl = 'https://finalopabackend.onrender.com';
-let idCounter = 0; // Contador para gerar IDs numéricos únicos
 
-// Enviar nova frase
+// Função para enviar uma nova frase
 function enviarNovaFrase() {
     const frase = document.getElementById('get-frase-textbox').value;
 
@@ -11,39 +11,45 @@ function enviarNovaFrase() {
         return;
     }
 
-    const id = idCounter++; // Use o valor atual do contador como ID e, em seguida, incremente
+    // Gera um ID único para a frase (pode ser feito de forma mais robusta)
+    const id = Date.now(); // Use a data e hora atual como ID
 
+    // Cria um objeto com o ID e a frase
+    const novaFrase = { id, frase };
+
+    // Envia a frase para a API
     fetch(`${apiUrl}/enviarfrases`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ id, frase }), // Inclui o ID no objeto enviado
+        body: JSON.stringify(novaFrase),
     })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            console.log(data); // Exibe a resposta da API no console
 
             // Limpa o campo de entrada
             document.getElementById('get-frase-textbox').value = '';
 
+            // Atualiza a lista de frases
             listarFrases();
         })
         .catch(error => console.error('Erro ao enviar a frase:', error));
 }
 
-function confirmarPostagem() {
-    const botaoEnviar = document.getElementById('botao-enviar');
-    botaoEnviar.addEventListener('click', enviarNovaFrase);
-}
-
+// Função para listar frases
 function listarFrases() {
     fetch(`${apiUrl}/listarfrases`)
         .then(response => response.json())
         .then(data => {
+            // Verifica se a resposta contém frases
             if (Array.isArray(data)) {
-                const frasesComIDs = data.map((item, index) => `ID: ${index} - ${item}`).join('\n');
-                exibirFrase(frasesComIDs);
+                // Formata as frases para exibição
+                const frasesFormatadas = data.map((frase, index) => `ID: ${frase.id} - ${frase.frase}`).join('\n');
+                
+                // Exibe as frases na caixa de texto
+                exibirFrase(frasesFormatadas);
             } else {
                 console.log("Nenhuma frase inusitada disponível no momento.");
             }
@@ -51,13 +57,17 @@ function listarFrases() {
         .catch(error => console.error('Erro ao listar frases:', error));
 }
 
-
-function exibirFrase(frase) {
-    var textarea = document.getElementById('exibir-frase-textbox');
-    textarea.value = frase;
+// Função para exibir frases na caixa de texto
+function exibirFrase(frases) {
+    const textarea = document.getElementById('exibir-frase-textbox');
+    textarea.value = frases;
 }
 
-confirmarPostagem();
+// Configura o botão de envio
+const botaoEnviar = document.getElementById('botao-enviar');
+botaoEnviar.addEventListener('click', enviarNovaFrase);
+
+// Inicializa a lista de frases
 listarFrases();
 
 // Função para deletar uma frase pelo ID
