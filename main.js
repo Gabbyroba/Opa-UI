00b386a86e27a025b51da73f47bef669dc8d5599
Gabbyroba <gabbyramosbr2@gmail.com>
2023-09-19 21:45:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,125 +1,100 @@
+// Variáveis para os elementos HTML
+const getFraseTextbox = document.getElementById("get-frase-textbox");
+const exibirFraseTextbox = document.getElementById("exibir-frase-textbox");
+const botaoEnviar = document.getElementById("botao-enviar");
+const fraseIdInput = document.getElementById("frase-id");
+const botaoDeletarFrase = document.getElementById("botao-deletar-frase");
+
 // URL da API Oracle
-const oracleApiUrl = "https://g2db51f405a4261-opadatabase.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/frases_inusitadas/";
+const apiUrl = "https://g2db51f405a4261-opadbo.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/frases_inusitadas/";
 
-// Função para enviar uma nova frase para a API Oracle
-function enviarNovaFrase() {
-    const frase = document.getElementById('get-frase-textbox').value;
-
-    // Verifica se a frase não está vazia
-    if (!frase.trim()) {
-        console.error('Frase inválida. Insira uma frase.');
-        return;
-    }
-
-    // Cria um objeto com a frase
-    const novaFrase = { frase };
-
-    // Envia a frase para a API Oracle
-    fetch(oracleApiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(novaFrase),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro ao enviar a frase.');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data.message); // Exibe a mensagem de sucesso
-        const id_real = data.id; // Obtém o ID real atribuído pela API
-
-        // Limpa o campo de entrada
-        document.getElementById('get-frase-textbox').value = '';
-
-        // Exibe a frase com o ID real na text-box 2
-        document.getElementById('exibir-frase-textbox').value = `ID: ${id_real} - ${frase}`;
-    })
-    .catch(error => console.error('Erro ao enviar a frase:', error));
-}
+// Variável para armazenar as frases formatadas
+let frasesFormatadas = [];
 
 // Função para listar todas as frases da API Oracle
-function listarFrases() {
-    fetch(`${oracleApiUrl}`)
-    .then(response => response.json())
-    .then(data => {
-        if (data.items && data.items.length > 0) {
-            const frasesComIDs = data.items.map(item => `ID: ${item.id} - ${item.frase}`).join('\n');
-            exibirFrase(frasesComIDs);
+async function listarFrasesDaAPI() {
+    try {
+        const response = await fetch(apiUrl, {
+            method: "GET",
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+
+            // Formate as frases no formato "Id 0000 - 'Frase de exemplo'"
+            frasesFormatadas = data.items.map((item) => {
+                return `Id ${item.id.toString().padStart(5, '0')} - "${item.frase}"`;
+            });
+
+            // Atualize a caixa de texto para exibir as frases formatadas
+            exibirFraseTextbox.value = frasesFormatadas.join('\n');
         } else {
-            console.log("Nenhuma frase inusitada disponível no momento.");
+            console.error("Erro ao listar frases:", response.statusText);
         }
-    })
-    .catch(error => console.error('Erro ao listar frases:', error));
-}
-
-// Função para exibir frases na caixa de texto
-function exibirFrase(frases) {
-    const textarea = document.getElementById('exibir-frase-textbox');
-    textarea.value = frases;
-}
-
-listarFrases();
-
-// Configura o botão de envio
-const botaoEnviar = document.getElementById('botao-enviar');
-botaoEnviar.addEventListener('click', () => { enviarNovaFrase, listarFrases
-});
-
-// Função para deletar uma frase da API Oracle por ID
-function deletarFrasePorID() {
-    const idFrase = parseInt(document.getElementById('frase-id').value); // Pegar o ID da entrada
-
-    if (!isNaN(idFrase) && idFrase >= 0) {
-        fetch(`${oracleApiUrl}/${idFrase}`, {
-            method: 'DELETE',
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao deletar a frase.');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data.message); // Exibe a mensagem de sucesso
-            listarFrases();
-        })
-        .catch(error => console.error('Erro ao deletar a frase:', error));
-    } else {
-        console.log('ID de frase inválido. Insira um número inteiro válido.');
+    } catch (error) {
+        console.error("Erro ao listar frases:", error);
     }
 }
 
-// Configura o botão de deletar frase
-const botaoDeletarFrase = document.getElementById('botao-deletar-frase');
-botaoDeletarFrase.addEventListener('click', deletarFrasePorID);
 
-// Inicializa a lista de frases
-listarFrases();
-
-// Função para copiar o link do servidor para a área de transferência
-function copyServerLink() {
-    const serverLink = "ouviporai.netlify.app/"; 
-    const tempTextArea = document.createElement("textarea");
-    
-    tempTextArea.value = serverLink;
-    
-    document.body.appendChild(tempTextArea);
-    tempTextArea.select();
+// Função para enviar uma nova frase para a API Oracle
+async function enviarFraseParaAPI() {
+    const novaFrase = {
+        id: Date.now(), // Gerando um ID único
+        frase: getFraseTextbox.value,
+    };
 
     try {
-        document.execCommand("copy");
-        alert("Link copiado para a área de transferência: " + serverLink);
-    } catch (err) {
-        console.error("Erro ao copiar o link: ", err);
-    } finally {
-        document.body.removeChild(tempTextArea);
+        const response = await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(novaFrase),
+        });
+
+        if (response.status === 201) {
+            // Envio bem-sucedido, limpa o campo de entrada após enviar a frase
+            getFraseTextbox.value = "";
+            // Chama a função para listar todas as frases novamente para atualizar a exibição
+            listarFrasesDaAPI();
+        } else {
+            console.error("Erro ao enviar frase:", response.statusText);
+        }
+    } catch (error) {
+        console.error("Erro ao enviar frase:", error);
     }
 }
 
-// Configura o botão de compartilhar
-const botaoCompartilhar = document.getElementById("botao-compartilhar");
-botaoCompartilhar.addEventListener("click", copyServerLink);
+// Função para deletar uma frase da API por ID
+async function deletarFrasePorIdNaAPI() {
+    const id = fraseIdInput.value;
+
+    try {
+        const response = await fetch(apiUrl + id, {
+            method: "DELETE",
+        });
+
+        if (response.status === 204 || response.status === 200) {
+            // Remove a frase da lista de frases formatadas
+            frasesFormatadas = frasesFormatadas.filter((frase) => !frase.includes(`Id ${id.toString().padStart(5, '0')} -`));
+
+            // Atualiza a caixa de texto de exibição com a lista atualizada
+            exibirFraseTextbox.value = frasesFormatadas.join('\n');
+
+            // Limpe o campo de ID
+            fraseIdInput.value = "";
+        } else {
+            console.error("Erro ao deletar frase:", response.statusText);
+        }
+    } catch (error) {
+        console.error("Erro ao deletar frase:", error);
+    }
+}
+
+// Adiciona event listeners para os botões
+botaoEnviar.addEventListener("click", enviarFraseParaAPI);
+botaoDeletarFrase.addEventListener("click", deletarFrasePorIdNaAPI);
+
+// Chama a função listarFrasesDaAPI para exibir todas as frases ao carregar a página
+listarFrasesDaAPI();
